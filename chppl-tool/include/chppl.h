2c2913e324d69323b1106b1032e41f2b7034030e
nocotan <noconoco.lib@gmail.com>
2016-06-06 10:44:30 +0900

    mv

---
+++ chppl-tool/include/chppl.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_CHPPL_H_
+#define INCLUDE_CHPPL_H_
+
+#include <iostream>
+#include "common_pg.h"
+
+#endif
---
+++ chppl-tool/src/chppl.cpp
@@ -0,0 +1,25 @@
+#include "../include/chppl.h"
+
+int main(int argc, char **argv) {
+  Cpgsql con(
+      "ec2-50-19-239-232.compute-1.amazonaws.com",
+      "agcdhswzpdwrbp",
+      "gurQBp9jT2t1eNMHI3P7Ew5g_0",
+      "d8rjp9952jhohi");
+
+  std::cout << "connect" << std::endl;
+  PGresult* res = con.m_ExecSql("SELECT * FROM libraries;");
+
+  if (NULL == res) {
+    return 1;
+  }
+
+  int n = PQnfields(res);
+  int rows = PQntuples(res);
+
+  std::cout << PQgetvalue(res, 1, PQfnumber(res, "name")) << std::endl;
+
+
+  PQclear(res);
+  return 0;
+}
